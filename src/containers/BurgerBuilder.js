ff7e8afe97b89e7a4f5e0e14f2294c2f6109b95c
Ola Assem <ola.assem@viget.com>
2020-12-17 14:09:31 +0200

    outputs burger ingredients dynamically

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -5,10 +5,20 @@ import Burger from '../components/Burger/Burger';
 
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {       //must use render() method with class components
         return (      // then return something
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div> Burger Controls</div>
             </Aux>
         ); 
