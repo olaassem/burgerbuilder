af68da2b50e5b29356a09a3f8dadc7e6c8674c6c
Ola Assem <ola.assem@viget.com>
2020-12-17 14:24:16 +0200

    calculates ing sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 return <BurgerIngredient key={ingKey + i} type={ingKey} />
             });
-        });
+        })
+        .reduce(( arr, el ) => {
+            return arr.concat(el)
+        }, []);
+
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Start building your burger.</p>
+        }
+
+    console.log(transformedIngredients);
 
     return ( 
         <div className={classes.Burger}>
