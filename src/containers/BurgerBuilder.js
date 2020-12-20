8cc67dbc740a0cf67631ba4fffe7affb94f33575
Ola Assem <ola.assem@viget.com>
2020-12-20 22:12:58 +0200

    connecting state to buildcontrols 1

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 import Aux from '../hoc/Aux';
 import Burger from '../components/Burger/Burger';
+import BuildControls from '../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 
 class BurgerBuilder extends Component {
@@ -13,13 +21,30 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         }
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredient[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
     }
 
     render () {       //must use render() method with class components
         return (      // then return something
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div> Burger Controls</div>
+                <BuildControls />
             </Aux>
         ); 
     }
