4a123c887255ed576ac86e7705c426d5788decec
Ola Assem <ola.assem@viget.com>
2021-03-04 14:49:59 +0200

    reusing backdrop & closing sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,27 @@ import React from 'react';
 import classes from './SideDrawer.css'
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-    //...
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavItems />
+                </nav>
             </div>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
  
