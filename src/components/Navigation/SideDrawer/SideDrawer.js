77fa9e8c349489d2cd2d97fa9131c13d0fe1bd47
Ola Assem <ola.assem@viget.com>
2021-03-04 12:48:18 +0200

    creates resp sidedrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './SideDrawer.css'
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
+
+const sideDrawer = (props) => {
+    //...
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavItems />
+            </nav>
+        </div>
+    );
+};
+ 
+export default sideDrawer;
