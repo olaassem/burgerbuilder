cc0b3e88a194463ca1a70c6e93fc3d35d4dabb40
Ola Assem <ola.assem@viget.com>
2021-03-04 15:11:36 +0200

    functioning sidedrawer toggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,13 @@ import React from 'react';
 import classes from '../Toolbar/Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div className={classes.Logo}>
+        <DrawerToggle clicked={props.drawerToggleClicked} className={classes.Logo}>
             <Logo />
-        </div>
+        </DrawerToggle>
         <nav className={classes.DesktopOnly}>
             <NavItems />
         </nav>
