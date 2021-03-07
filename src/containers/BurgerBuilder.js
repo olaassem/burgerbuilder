a539c2bb222353eb88fae721f1219b14c2581975
Ola Assem <ola.assem@viget.com>
2021-03-07 14:14:49 +0200

    displaying a spinner when sending a request

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Burger from '../components/Burger/Burger';
 import BuildControls from '../components/Burger/BuildControls/BuildControls';
 import Modal from '../components/UI/Modal/Modal';
 import OrderSummary from '../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../components/UI/Spinner/Spinner';
 
 
 const INGREDIENT_PRICES = {
@@ -15,9 +16,7 @@ const INGREDIENT_PRICES = {
     bacon: 1
 }
 
-
 class BurgerBuilder extends Component {
-
     state = {
         ingredients: {
             salad: 0,
@@ -27,11 +26,11 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 5,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState = (ingredients) => {
-
         const sum = Object.keys(ingredients)
             .map(igKey => {
                 return ingredients[igKey] 
@@ -42,7 +41,6 @@ class BurgerBuilder extends Component {
         this.setState({purchasable: sum > 0});
     }
 
-    
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
@@ -57,7 +55,6 @@ class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingredients: updatedIngredients
         });
-    
         this.updatePurchaseState(updatedIngredients);
     }
 
@@ -91,6 +88,7 @@ class BurgerBuilder extends Component {
     }
     
     purchaseContinueHandler = () => {
+        this.setState({loading: true});
         //alert('Continue');
         //need to add .json for firebase endpoints
         const order = { 
@@ -109,8 +107,18 @@ class BurgerBuilder extends Component {
         }
 
         axios.post('/orders.json', order)
-            .then( response => console.log(response))
-            .catch( error => console.log(error));
+            .then( response => {
+                this.setState({
+                    loading: false, 
+                    purchasing: false
+                });
+            })
+            .catch( error => {
+                this.setState({
+                    loading: false, 
+                    purchasing: false
+                });
+            })
     }
 
     render () {       //must use render() method with class components
@@ -123,15 +131,22 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
 
-
-        return (      // then return something
-            <Aux>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary
+        let orderSummary =  <OrderSummary
                         ingredients={this.state.ingredients}
                         price={this.state.totalPrice}
                         purchaseCanceled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}/>
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
+        }
+
+
+        return (      // then return something
+            <Aux>
+                <Modal 
+                    show={this.state.purchasing} 
+                    modalClosed={this.purchaseCancelHandler}>
+                        {orderSummary}
                 </Modal>
                 <Burger 
                     ingredients={this.state.ingredients} 
