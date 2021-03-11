07bf07f056017b49785de378fecb47adaa0cc62f
Ola Assem <ola.assem@viget.com>
2021-03-11 11:56:55 +0200

    checkout.js container

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,6 +4,21 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
 
+    state= {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            meat: 1,
+            cheese: 1
+        }
+    }
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}/>
+            </div>
+        )
+    }
 }
 
 export default Checkout;
