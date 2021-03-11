33418bc14c65023f4769718ad21c477c6e6173c0
Ola Assem <ola.assem@viget.com>
2021-03-11 12:06:44 +0200

    rendering the checkout summary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -3,13 +3,13 @@ import React from 'react';
 import classes from './CheckoutSummary.css';
 
 import Burger from '../../Burger/Burger';
-import Buttons from '../../UI/Button/Button';
+import Button from '../../UI/Button/Button';
 
 const checkOutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope you enjoy your burger.</h1>
-            <div style={{width: '300px', height: '300px', margin:'auto'}}>
+            <div style={{width: '100%', margin:'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button 
