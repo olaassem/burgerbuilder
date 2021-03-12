816642c8902edd8642368ccfc7df27438063394c
Ola Assem <ola.assem@viget.com>
2021-03-12 11:44:31 +0200

    passing query ing thru query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -16,9 +16,21 @@ class Checkout extends Component {
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
+
+    
     
     checkoutContinuedHandler = () => {
-        this.props.history.replace('/checkout/contact-data');
+        const queryParams = [];
+        for ( let i in this.state.ingredients ) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        const queryString = queryParams.join('&');
+
+        this.props.history.replace({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
 
