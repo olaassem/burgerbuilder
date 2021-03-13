e6a48ac56c408b9bbd67e89c9693c5ba82b9ea44
Ola Assem <ola.assem@viget.com>
2021-03-13 19:39:08 +0200

    adds error handler

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,37 @@
 import React, {Component} from 'react';
 
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount () {
+        axios.get('/.json')
+            .then( response => {
+                //we get back an obj with ids as properties
+                //we want to turn it into an array
+                console.log(response.data);
+                const fetchedOrders= [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ 
+                    loading: false,
+                    orders: fetchedOrders 
+                });
+            })
+            .catch( error => {
+                this.setState({ loading: false });
+            })
+    }
+
     render() { 
         return ( 
             <div>
@@ -13,4 +42,4 @@ class Orders extends Component {
     }
 }
  
-export default Orders;
+export default withErrorHandler(Orders, axios);
