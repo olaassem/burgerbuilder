740ff4d49335dff393065c27445c6ef4fce50ff0
Ola Assem <ola.assem@viget.com>
2021-03-13 19:10:18 +0200

    updates navitems links for routing

--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavItem.css'
 
 const navItem = (props) => ( 
     <li className={classes.NavItem}>
-        <a 
-            href={props.link}
-            className={props.active ? classes.active : null}>
+        <NavLink to={props.link}>
             {props.children}
-        </a>
+        </NavLink>
     </li>
  );
 
--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -5,8 +5,8 @@ import NavItem from './NavItem/NavItem';
 
 const navItems = () => (
     <ul className={classes.NavItems}>
-        <NavItem link="/" active>BurgerBuilder</NavItem>
-        <NavItem link="/">Checkout</NavItem>
+        <NavItem link="/">BurgerBuilder</NavItem>
+        <NavItem link="/orders">Orders</NavItem>
     </ul>
  );
 
