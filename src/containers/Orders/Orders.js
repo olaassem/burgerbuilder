7b759d2a97abc3110f0be7e499e1af6bb974282f
Ola Assem <ola.assem@viget.com>
2021-03-13 18:54:57 +0200

    orders and order files

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import classes from './Orders.css';
+
+class Orders extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Orders;
