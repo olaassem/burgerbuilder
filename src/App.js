4888d7f6b6729578ead93a17d683853ec4792132
Ola Assem <ola.assem@viget.com>
2021-03-13 19:06:00 +0200

    adds order route to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-// import BurgerIngredient from './components/Burger/BurgerIngredient/BurgerIngredient';
+import Orders from './containers/Orders/Orders';
 
 
 class App extends Component {
@@ -14,6 +14,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,12 +3,13 @@ import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
-
-    this.state = {  }
-    
-}
     render() { 
-        return (  );
+        return ( 
+            <div>
+                <Order />
+                <Order />
+            </div>
+         );
     }
 }
  
