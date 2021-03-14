58374a44ebf9aef00fa85736c119750084110455
Ola Assem <ola.assem@viget.com>
2021-03-14 19:47:40 +0200

    rendering price

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,7 @@ import classes from './Order.css';
 const order = ( props ) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 15.00</strong></p>
+        <p>Price: <strong>USD { Number.parseFloat(props.price).toFixed(2) }</strong></p>
     </div>
 )
  
