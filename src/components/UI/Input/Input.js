5cd0b968ccab70cbd061c0a215463d4dc566bcc4
Ola Assem <ola.assem@viget.com>
2021-03-15 17:10:53 +0200

    adds the dropdown select el to form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -21,6 +21,21 @@ const input = ( props ) => {
                 defaultValue={props.value} 
             />;
             break;
+
+        case ( 'select' ):
+            inputElement = (
+                <select 
+                    className={classes.InputElement}
+                    defaultValue={props.value} 
+                >
+                    { props.elementConfig.options.map( option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
         
         default:
             inputElement = <input 
